2625b0a2fc16c290684b70851e732aa2f69a6ef2
zaharlol <kovtun.zahar12@mail.ru>
2023-05-20 13:15:53 +0300

    13.2.6 | 13.2.5

--- Сollections/Program.cs
+++ Сollections/Program.cs
@@ -1,4 +1,7 @@
-﻿using System.Globalization;
+﻿using System.Collections;
+using System.Globalization;
+using System.Text;
+
 class Program
 {
     static void Main(string[] args)
@@ -9,6 +12,12 @@ class Program
 
         ReadFile readFile = new ReadFile();
         readFile.ReadFile1();
+
+        Months months = new Months();
+        months.Months1();
+
+        ArrayEx arrayEx = new ArrayEx();
+        arrayEx.ArrayEx1();
     }
 }
 class ReadFile
@@ -31,5 +40,71 @@ public class PM
                 return false;
         }
         return true;
+    }
+}
+class ArrayEx
+{
+    public void ArrayEx1()
+    {
+        var example = new ArrayList()
+           {
+               1,
+               "Самая ",
+               "лучшая ",
+               "коллекция",
+               300,
+           };
+
+        int sum = 0;
+
+        StringBuilder text = new StringBuilder();
+
+        foreach (var item in example) 
+        {
+            if (item is int)
+            {
+                sum += (int)item;
+            }
+        }
+        foreach (var item in example)
+        {
+            if (item is string)
+            {
+                text.Append(item);
+            }
+        }
+
+        var result = new ArrayList {sum, text.ToString()} ;
+
+        foreach (var item in result)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
+class Months
+{
+    public void Months1()
+    {
+        var months = new[]
+{
+   "Jan", "Feb", "Mar", "Apr", "May" , "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+        };
+
+        var numbers = new[]
+        {
+   1, 2, 3, 5, 6, 7, 8, 9, 10, 11, 12
+        };
+
+        var combinedList = new ArrayList();
+
+        foreach (var number in numbers)
+        {
+            combinedList.Add(months[number - 1]);
+            combinedList.Add(number);
+        }
+
+        foreach (var value in combinedList)
+            Console.WriteLine(value);
     }
 }
