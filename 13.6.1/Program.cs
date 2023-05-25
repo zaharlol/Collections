5f12c646096e20f08418d022b945c30b63ce9f2c
zaharlol <kovtun.zahar12@mail.ru>
2023-05-25 18:42:16 +0300

    13.6.1

---
+++ 13.6.1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Diagnostics;
+using System.Net;
+
+class Program
+    {
+        static void Main(string[] args)
+        {
+        StopWatch stopWatch = new StopWatch();
+
+        string path = File.ReadAllText("D:/VS/Новая папка/Text.txt");
+
+        List<string> list = new List<string>();
+        list.Add(path);
+        stopWatch.Estimate(list.Count);
+
+        LinkedList<string> strings = new LinkedList<string>();
+        strings.AddFirst(path);
+        stopWatch.Estimate(list.Count);
+    }
+    }
+class StopWatch
+{
+    static void CreateMatrix(int n)
+    {
+        var matrix = new int[n][];
+
+        for (int i = 0; i < n; i++)
+        {
+            matrix[i] = new int[n];
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                matrix[i][j] = i + j;
+            }
+        }
+    }
+
+    public void Estimate(int n)
+    {
+        var timer = new Stopwatch();
+        timer.Start();
+
+        for (int i = 0; i < n; i++)
+        {
+            timer.Restart();
+
+            CreateMatrix(10000);
+
+            timer.Stop();
+            Console.WriteLine(timer.ElapsedMilliseconds);
+        }
+    }
+}
---
+++ 13.6.2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using static System.Net.Mime.MediaTypeNames;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string text = File.ReadAllText("D:\\VS\\Новая папка\\Text.txt");
+        var noPunctuationText = new string(text.Where(c => !char.IsPunctuation(c)).ToArray());
+        var words = text.Split(noPunctuationText, StringSplitOptions.RemoveEmptyEntries);
+        Console.WriteLine(words.Length);
+    }
+}
